81194a09815df1c0a69d9c2591ee603dc2cc90f7
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2021-02-26 18:17:39 +0500

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,4 +1,4 @@
-//A node class which creates the nodes for the linked List along basic getter and setter functions
+//A node class which creates the nodes for the Post Expression along basic getter and setter functions
 
 #include<iostream>
 using namespace std;
