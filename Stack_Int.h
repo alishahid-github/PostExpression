fd434bdb560250d2cf539a0e11598c5d61742866
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2021-03-17 17:58:43 +0500

    Stack Header file for integers

---
+++ Stack_Int.h
@@ -0,0 +1,17 @@
+#include<iostream>
+#include"LIst_Int.h"
+#ifndef STACKK
+#define STACKK
+using namespace std;
+class stack
+{
+private:
+	list L1;
+public:
+	void push(int d);
+	int pop();
+	void print();
+};
+
+
+#endif
